f06e42bd90d1a317509e4184a023e903f507c680
VsevolodTheDemiurge <128226824+VsevolodTheDemiurge@users.noreply.github.com>
2023-10-08 22:34:16 +0300

    s

--- js/notifButtonScript.js
+++ js/notifButtonScript.js
@@ -7,9 +7,11 @@ imageButton.addEventListener('click', function() {
         // Если текущее изображение - image1.jpg, то меняем на image2.jpg
         imageButton.src = './imgs/notifications-icon-active.svg';
         imageButton.alt = 'Изображение 2';
+        imageButton.setAttribute('style', 'border-top-left-radius: 15px; border-top-right-radius: 15px; background: #c4cdd4;')
     } else {
         // В противном случае меняем обратно на image1.jpg
         imageButton.src = './imgs/notifications-icon.svg';
         imageButton.alt = 'Изображение 1';
+        imageButton.setAttribute('style', 'border-top-left-radius: 0px; border-top-right-radius: 0px; background: lightgray;')
     }
 });
