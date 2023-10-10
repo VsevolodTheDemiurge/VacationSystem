2c9f33eb13470528211d3b76cfbb9b4f7bb4ea55
VsevolodTheDemiurge <128226824+VsevolodTheDemiurge@users.noreply.github.com>
2023-10-10 21:15:52 +0300

    j

--- js/notifButtonScript.js
+++ js/notifButtonScript.js
@@ -1,16 +1,38 @@
 // Получаем ссылку на элемент кнопки с изображением
 const imageButton = document.getElementById('openNotifications');
 
+let unactiveImagePath;
+let activeImagePath;
+const currentPath = window.location.pathname;
+
+if (currentPath.endsWith('index.html')){
+    activeImagePath = './imgs/notifications-icon-active.svg';
+    unactiveImagePath = './imgs/notifications-icon.svg';
+}
+else if (currentPath.endsWith('/authorization-page.html')){
+    activeImagePath = '../imgs/notifications-icon-active.svg';
+    unactiveImagePath = '../imgs/notifications-icon.svg';
+}/*
+else if (currentPath.endsWith('/authorization-page.html')){
+    activeImagePath = '../imgs/notifications-icon-active.svg';
+    unactiveImagePath = '../imgs/notifications-icon.svg';
+}
+else if (currentPath.endsWith('/authorization-page.html')){
+    activeImagePath = '../imgs/notifications-icon-active.svg';
+    unactiveImagePath = '../imgs/notifications-icon.svg';
+}
+*/
+
 // Устанавливаем обработчик события "click" для кнопки
 imageButton.addEventListener('click', function() {
 	if (imageButton.src.endsWith('notifications-icon.svg')) {
         // Если текущее изображение - image1.jpg, то меняем на image2.jpg
-        imageButton.src = './imgs/notifications-icon-active.svg';
+        imageButton.src = activeImagePath;
         imageButton.alt = 'Изображение 2';
         imageButton.setAttribute('style', 'border-top-left-radius: 15px; border-top-right-radius: 15px; background: #c4cdd4;')
     } else {
         // В противном случае меняем обратно на image1.jpg
-        imageButton.src = './imgs/notifications-icon.svg';
+        imageButton.src = unactiveImagePath;
         imageButton.alt = 'Изображение 1';
         imageButton.setAttribute('style', 'border-top-left-radius: 0px; border-top-right-radius: 0px; background: lightgray;')
     }
